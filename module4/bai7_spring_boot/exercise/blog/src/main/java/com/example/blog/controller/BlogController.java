1e34fa90bf57ccf7ad89c7c64244ebb758453701
Truong Quoc phap <truongquocphap@MacBook-Pro-cua-Truong.local>
2021-07-24 22:53:23 +0700

    upbai

--- module4/bai7_spring_boot/exercise/blog/src/main/java/com/example/blog/controller/BlogController.java
+++ module4/bai7_spring_boot/exercise/blog/src/main/java/com/example/blog/controller/BlogController.java
@@ -69,50 +69,5 @@ public class BlogController {
         iBlogService.delete(id);
         return "redirect:/";
     }
-    @GetMapping("show-category")
-    public String showCategory(@PageableDefault(value = 3) Pageable pageable,
-                           Model model){
-        model.addAttribute("categoryList",iCategoryService.findAll(pageable));
-        return "category/list";
-    }
-    @GetMapping("create-category")
-    public String createCategory(Model model){
-        model.addAttribute("category",new Category());
-        return "category/create";
-    }
-    @PostMapping("create-category")
-    public String createCategory(@ModelAttribute("category") Category category,Model model){
-        model.addAttribute("blog",new Category());
-        iCategoryService.save(category);
-        return "redirect:/show-category";
-    }
-    @GetMapping("delete-category")
-    public String deleteCategory(@RequestParam("id") Integer id){
-        iCategoryService.delete(id);
-        return "redirect:/show-category";
-    }
-    @GetMapping("edit-category/{id}")
-    public String createCategory(@PathVariable("id") Integer id, Model model){
-        Category category=iCategoryService.findById(id);
-        model.addAttribute("category",category);
-        return "category/edit";
-    }
-    @PostMapping("edit-category")
-    public String editCategory(@ModelAttribute("category") Category category,Model model){
-        iCategoryService.save(category);
-        return "redirect:/show-category";
-    }
-    @GetMapping("blog-list-category/{id}")
-    public String blogListCategory( @PathVariable("id") Integer id,
-                           Model model){
-        model.addAttribute("blogList",iBlogService.findAllByCategory_Id(id));
-        return "category/blog_list_category";
-    }
-    @GetMapping("view/{id}")
-    public String viewBlog( @PathVariable("id") Integer id ,
-                           Model model){
-        Blog blog=iBlogService.findById(id);
-        model.addAttribute("blogList",blog);
-        return "blog/view";
-    }
+
 }
