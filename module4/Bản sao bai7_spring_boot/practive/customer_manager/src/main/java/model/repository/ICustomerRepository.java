cdf80bcb1f699d4fb0ffdffbda6478f27b5473fd
Truong Quoc phap <truongquocphap@MacBook-Pro-cua-Truong.local>
2021-07-24 17:48:24 +0700

    upbai

--- module4/Bản sao bai7_spring_boot/practive/customer_manager/src/main/java/model/repository/ICustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package model.repository;
-import model.bean.Customer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ICustomerRepository extends PagingAndSortingRepository<Customer,Long > {
-    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
-}
