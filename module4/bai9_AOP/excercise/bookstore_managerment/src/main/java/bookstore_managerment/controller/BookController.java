0fdc54e29e5af49bd260bdd6aa63508689d76c78
Truong Quoc phap <truongquocphap@MacBook-Pro-cua-Truong.local>
2021-07-29 22:58:04 +0700

    upbai

--- module4/bai9_AOP/excercise/bookstore_managerment/src/main/java/bookstore_managerment/controller/BookController.java
+++ module4/bai9_AOP/excercise/bookstore_managerment/src/main/java/bookstore_managerment/controller/BookController.java
@@ -12,17 +12,20 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
     @Autowired
     IBookService iBookService;
+
     @GetMapping("/book")
     public String listBook(Model model) {
 
         model.addAttribute("book", iBookService.findAll());
         return "book/list_book";
     }
+
     @GetMapping("view/{id}")
     public String viewBook(@PathVariable("id") Integer id, Model model) {
         model.addAttribute("book", iBookService.findById(id));
         return "book/view_book";
     }
+
     @PostMapping("/rent_book")
     public String rent(@RequestParam("id") Integer id, Model model) throws BookMountException {
         Book book = iBookService.findById(id);
@@ -35,6 +38,7 @@ public class BookController {
             return "book/list_book";
         }
     }
+
     @GetMapping("give_book")
     public String showGiveBook(Model model) {
         return "book/give_book";
@@ -48,6 +52,7 @@ public class BookController {
         model.addAttribute("book", iBookService.findAll());
         return "book/list_book";
     }
+    
     @ExceptionHandler(BookMountException.class)
     public String showError() {
         return "book/error";
