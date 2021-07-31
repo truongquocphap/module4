b5a47fec43f310e3275c72c2e5c2140bf9f76c3b
Truong Quoc phap <truongquocphap@MacBook-Pro-cua-Truong.local>
2021-08-01 00:49:14 +0700

    upbai

---
+++ module4/bai12_ajax/practice/smart_phone/src/main/java/smart_phone/controller/SmartphoneRestController.java
@@ -0,0 +1,45 @@
+package smart_phone.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import smart_phone.model.entity.Smartphone;
+import smart_phone.model.repository.ISmartphoneRepository;
+
+import java.util.Optional;
+
+@RestController
+public class SmartphoneRestController {
+    @Autowired
+    private ISmartphoneRepository smartphoneService;
+
+    @PostMapping
+    public ResponseEntity<Smartphone> createSmartphone(@RequestBody Smartphone smartphone) {
+        return new ResponseEntity<>(smartphoneService.save(smartphone), HttpStatus.CREATED);
+    }
+    @GetMapping("/list")
+    public ModelAndView getAllSmartphonePage() {
+        ModelAndView modelAndView = new ModelAndView("/list");
+        modelAndView.addObject("smartphones", smartphoneService.findAll());
+        return modelAndView;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Smartphone> deleteSmartphone(@PathVariable Long id) {
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        smartphoneService.deleteById(id);
+        return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<Smartphone> updateSmartphone(@RequestBody Smartphone smartphone) {
+        return new ResponseEntity<>(smartphoneService.save(smartphone), HttpStatus.CREATED);
+    }
+}
