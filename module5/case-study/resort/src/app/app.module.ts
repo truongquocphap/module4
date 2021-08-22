9745d840588197b03d13a552d4250d192e35e856
Truong Quoc phap <truongquocphap@MacBook-Pro-cua-Truong.local>
2021-08-22 22:26:09 +0700

    upbai

--- module5/case-study/resort/src/app/app.module.ts
+++ module5/case-study/resort/src/app/app.module.ts
@@ -8,13 +8,48 @@ import { HeaderComponent } from './layout/header/header.component';
 import { ContentComponent } from './layout/content/content.component';
 import { HomeComponent } from './layout/home/home.component';
 
+import { ContractListComponent } from './contract/contract-list/contract-list.component';
+import { ContractCreateComponent } from './contract/contract-create/contract-create.component';
+import { ContractUpdateComponent } from './contract/contract-update/contract-update.component';
+import { ContractViewComponent } from './contract/contract-view/contract-view.component';
+import { ContractDetailListComponent } from './contract-detail/contract-detail-list/contract-detail-list.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { CustomerUpdateComponent } from './customer/customer-update/customer-update.component';
+import { CustomerCreateComponent } from './customer/customer-create/customer-create.component';
+import { CustomerViewComponent } from './customer/customer-view/customer-view.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeUpdateComponent } from './employee/employee-update/employee-update.component';
+import { EmployeeCreateComponent } from './employee/employee-create/employee-create.component';
+import { EmployeeViewComponent } from './employee/employee-view/employee-view.component';
+import { ServiceListComponent } from './service/service-list/service-list.component';
+import { ServiceCreateComponent } from './service/service-create/service-create.component';
+import { ServiceUpdateComponent } from './service/service-update/service-update.component';
+import { ServiceViewComponent } from './service/service-view/service-view.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
     ContentComponent,
-    HomeComponent
+    HomeComponent,
+    ContractListComponent,
+    ContractCreateComponent,
+    ContractUpdateComponent,
+    ContractViewComponent,
+    ContractDetailListComponent,
+    CustomerListComponent,
+    CustomerUpdateComponent,
+    CustomerCreateComponent,
+    CustomerViewComponent,
+    EmployeeListComponent,
+    EmployeeUpdateComponent,
+    EmployeeCreateComponent,
+    EmployeeViewComponent,
+    ServiceListComponent,
+    ServiceCreateComponent,
+    ServiceUpdateComponent,
+    ServiceViewComponent
   ],
   imports: [
     BrowserModule,
